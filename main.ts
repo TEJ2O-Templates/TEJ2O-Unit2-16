3851506e001290944129c61bd9656c4aca2b26ec
Patrick Coxall <mr.coxall@mths.ca>
2023-12-04 09:38:28 -0500

    init

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
+
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+
+
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program uses the bluetooth radios
+ * Also, using the new standard of using a while (true) statement
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(1)
+basic.showIcon(IconNames.Happy)
+
+// loop forever
+while (true) {
+    if (input.buttonIsPressed(Button.A) == true) {
+        radio.sendString("Hello, World!")
+    }
+    radio.onDataReceived(() => {
+        basic.clearScreen()
+        let message: string = radio.receiveString()
+        basic.showString(message)
+        basic.showIcon(IconNames.Happy)
+    })
+}
